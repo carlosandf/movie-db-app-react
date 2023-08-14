c15762d11357c5e19e5d8393066b9c0f2c8f5be9
carlosandf <carlos.andres.ff2912@gmail.com>
2023-08-13 23:31:33 -0500

    Fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', 'node_modules'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname
+  },
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
